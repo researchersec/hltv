ec46a845d762ab17b9d1277ce395cc9a95d751f7
researchersec <109282836+researchersec@users.noreply.github.com>
2024-03-04 17:50:17 +0100

    Create scraper.py

---
+++ scraper.py
@@ -0,0 +1,20 @@
+import requests
+
+# URL of the FlareSolverr API endpoint
+url = "http://localhost:8191/v1"
+
+# Headers for the POST request
+headers = {"Content-Type": "application/json"}
+
+# Data to be sent in the POST request
+data = {
+    "cmd": "request.get",       # Command to indicate that you want to perform a GET request
+    "url": "http://www.google.com/",  # URL you want to request
+    "maxTimeout": 60000         # Maximum timeout in milliseconds (optional)
+}
+
+# Send the POST request to the FlareSolverr API endpoint
+response = requests.post(url, headers=headers, json=data)
+
+# Print the response
+print(response.text)
