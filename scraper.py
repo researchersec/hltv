c2d5cb88b4ae0e5e46b33585950687e5a8a6ec43
researchersec <109282836+researchersec@users.noreply.github.com>
2024-03-04 17:55:32 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 import requests
 
 # URL of the FlareSolverr API endpoint
-url = "http://localhost:8191/v1"
+url = "http://flaresolverr-container:8191/v1"
 
 # Headers for the POST request
 headers = {"Content-Type": "application/json"}
