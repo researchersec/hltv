3618b45659182f24197678388644cc64c6e73129
researchersec <109282836+researchersec@users.noreply.github.com>
2024-03-06 16:43:06 +0100

    Update demos.py

--- demos.py
+++ demos.py
@@ -1,35 +1,49 @@
 import requests
 import json
 import os
- 
+import zipfile
+
 url_cookie = "https://hltv.org/results"
 url_demo = "https://www.hltv.org/download/demo/56508"
 api_url = "http://localhost:8191/v1"
 headers = {"Content-Type": "application/json"}
 
-filename = url_demo.split("/")[-1]+".rar"  # Extracting filename from URL
+filename = url_demo.split("/")[-1]  # Extracting filename from URL
 
 data = {
     "cmd": "request.get",
     "url": url_cookie,
     "maxTimeout": 60000
 }
- 
+
 response = requests.post(api_url, headers=headers, json=data)
 
 # retrieve the entire JSON response from FlareSolverr
 response_data = json.loads(response.content)
- 
+
 # Extract the cookies from the FlareSolverr response
 cookies = response_data["solution"]["cookies"]
 
 # Clean the cookies
 cookies = {cookie["name"]: cookie["value"] for cookie in cookies}
- 
+
 # Extract the user agent from the FlareSolverr response
 user_agent = response_data["solution"]["userAgent"]
 
-demo_file = requests.get(url_demo, cookies=cookies, headers={"User-Agent": user_agent}) 
+demo_file = requests.get(url_demo, cookies=cookies, headers={"User-Agent": user_agent})
+
+# Save the demo file
 with open(filename, "wb") as f:
     f.write(demo_file.content)
 print(f"Demo downloaded successfully to {filename}")
+
+# Compress the file
+zip_filename = filename + ".zip"
+with zipfile.ZipFile(zip_filename, 'w') as zipf:
+    zipf.write(filename, os.path.basename(filename))
+
+print(f"File compressed successfully to {zip_filename}")
+
+# Optionally, you can remove the original file
+os.remove(filename)
+print(f"Original file {filename} removed.")
