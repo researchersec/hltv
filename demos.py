944897826a77f002c6009d06aec830c712cd9781
researchersec <109282836+researchersec@users.noreply.github.com>
2024-03-09 17:17:04 +0100

    Update demos.py

--- demos.py
+++ demos.py
@@ -220,7 +220,34 @@ def get_results_with_demo_links():
 
 
 def download_extract_compress(demo_link):
-    response = requests.get(demo_link)
+    url_cookie = "https://hltv.org/results"
+    #url_demo = "https://www.hltv.org/download/demo/56508"
+    api_url = "http://localhost:8191/v1"
+    headers = {"Content-Type": "application/json"}
+
+    filename = demo-link.split("/")[-1]  # Extracting filename from URL
+
+    data = {
+        "cmd": "request.get",
+        "url": url_cookie,
+        "maxTimeout": 60000
+    }
+
+    response = requests.post(api_url, headers=headers, json=data)
+
+# retrieve the entire JSON response from FlareSolverr
+    response_data = json.loads(response.content)
+
+# Extract the cookies from the FlareSolverr response
+    cookies = response_data["solution"]["cookies"]
+
+# Clean the cookies
+    cookies = {cookie["name"]: cookie["value"] for cookie in cookies}
+
+# Extract the user agent from the FlareSolverr response
+    user_agent = response_data["solution"]["userAgent"]
+
+    demo_file = requests.get(demo-link, cookies=cookies, headers={"User-Agent": user_agent})
     if response.status_code == 200:
         filename = demo_link.split("/")[-1]
         with open(filename, "wb") as f:
