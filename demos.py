9d7d354f8d0b568a61477202db296fa4bd08bdf4
researchersec <109282836+researchersec@users.noreply.github.com>
2024-03-21 15:30:22 +0100

    Update demos.py

--- demos.py
+++ demos.py
@@ -279,32 +279,37 @@ def download_demo_file(demo_link, api_url=FLARE_SOLVERR_URL):
         # Parse and process the extracted files
         for file in extracted_files:
             # Assuming 'file' is the path to the extracted file
-            parser = DemoParser(f"extracted_files/{file}")
-            print("Parsing started")
-            event_df = parser.parse_event("player_death", player=["X", "Y"], other=["total_rounds_played"])
-            #ticks_df = parser.parse_ticks(["X", "Y"])
-            file_hash = compute_file_hash(f"extracted_files/{file}")
-            event_output_dir = f"events/{file_hash}"
-            #ticks_output_dir = f"ticks/{file_hash}"
-
-            print("Parsing finished")
-            # Save event_df and ticks_df to JSON files
-            os.makedirs(event_output_dir, exist_ok=True)
-            #os.makedirs(ticks_output_dir, exist_ok=True)
-            event_df.to_json(f'{event_output_dir}/{file_hash}.json', indent=4)
-            #ticks_df.to_json(f'{ticks_output_dir}/{file_hash}.json', indent=4)
-
-            print("Parsed file saved")
-            # Compress the JSON files using xz
-            subprocess.run(["xz", f"{event_output_dir}/{file_hash}.json"])
-            #subprocess.run(["xz", f"{ticks_output_dir}/{file_hash}.json"])
-
-            # Execute the 'ls' command and capture the output
-            output = subprocess.check_output(['ls']).decode('utf-8')
-            print(output)
-            # Delete the JSON files
-            #os.remove(f'{event_output_dir}/{file_hash}.json')
-            #os.remove(f'{ticks_output_dir}/{file_hash}.json')
+            if file.endswith('.dem'):
+                parser = DemoParser(f"extracted_files/{file}")
+                # Proceed with parsing and processing
+            
+                print("Parsing started")
+                event_df = parser.parse_event("player_death", player=["X", "Y"], other=["total_rounds_played"])
+                #ticks_df = parser.parse_ticks(["X", "Y"])
+                file_hash = compute_file_hash(f"extracted_files/{file}")
+                event_output_dir = f"events/{file_hash}"
+                #ticks_output_dir = f"ticks/{file_hash}"
+    
+                print("Parsing finished")
+                # Save event_df and ticks_df to JSON files
+                os.makedirs(event_output_dir, exist_ok=True)
+                #os.makedirs(ticks_output_dir, exist_ok=True)
+                event_df.to_json(f'{event_output_dir}/{file_hash}.json', indent=4)
+                #ticks_df.to_json(f'{ticks_output_dir}/{file_hash}.json', indent=4)
+    
+                print("Parsed file saved")
+                # Compress the JSON files using xz
+                subprocess.run(["xz", f"{event_output_dir}/{file_hash}.json"])
+                #subprocess.run(["xz", f"{ticks_output_dir}/{file_hash}.json"])
+    
+                # Execute the 'ls' command and capture the output
+                output = subprocess.check_output(['ls']).decode('utf-8')
+                print(output)
+                # Delete the JSON files
+                #os.remove(f'{event_output_dir}/{file_hash}.json')
+                #os.remove(f'{ticks_output_dir}/{file_hash}.json')
+            else:
+                print(f"Ignoring file {file} because it's not a .dem file")
 
         # Delete the extracted files
         shutil.rmtree("extracted_files")
