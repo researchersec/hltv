ae7f1c3fda2828d5f857ce2c38fb0bfbf13342d0
researchersec <109282836+researchersec@users.noreply.github.com>
2024-04-01 00:01:04 +0200

    Fixing checks

--- demos.py
+++ demos.py
@@ -212,20 +212,18 @@ def get_results_with_demo_links():
                 elif "(LAN)" in tourney_mode_data:
                     result["tourney-mode"] = "lan"
                 event_directory = os.path.join(root_directory, result['tourney-mode'], result['event'])
-                
+            
                 # Check if the event directory exists before proceeding
                 if os.path.exists(event_directory):
-                    print(f"Event directory {event_directory} does exist. Skipping...")
-                    continue  # Skip to the next result
+                    # Get a list of all directories in the event directory
+                    all_directories = [d for d in os.listdir(event_directory) if os.path.isdir(os.path.join(event_directory, d))]
+                    # Check if any directory starts with the match ID
+                    match_directory_exists = any(dir.startswith(str(result['match-id'])) for dir in all_directories)
 
-                #directories = [d for d in os.listdir(event_directory) if os.path.isdir(os.path.join(event_directory, d))]
-               # match_directory_exists = any(dir.startswith(str(result['match-id'])) for dir in directories)
-
-                #print(f"Checking if demo directory exists for match {result['match-id']}")
-                #if match_directory_exists:
-                    #print(f"Demo for match {result['match-id']} already saved. Skipping.")
-                    #continue  # Skip downloading the demo
-                # Download, extract, and compress the demo file
+                    if match_directory_exists:
+                        print(f"A directory starting with match ID {result['match-id']} exists. Skipping...")
+                        continue  # Skip to the next result
+                        
                 if demo_link:
                     demo_link = "https://www.hltv.org"+demo_link
                     print(demo_link)
